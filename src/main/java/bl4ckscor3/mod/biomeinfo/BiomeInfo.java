90339b1046bf3008e4731c95cdd321b9377c3832
SalveMundiProd <salvemundiproductions@gmail.com>
2021-11-05 10:43:23 -0400

    updates

--- src/main/java/bl4ckscor3/mod/biomeinfo/BiomeInfo.java
+++ src/main/java/bl4ckscor3/mod/biomeinfo/BiomeInfo.java
@@ -31,10 +31,22 @@ public class BiomeInfo implements ClientModInitializer
 						if(biome != null)
 						{
 							TranslatableText biomeName = new TranslatableText(Util.createTranslationKey("biome", mc.world.getRegistryManager().get(Registry.BIOME_KEY).getId(biome)));
-
+							String biomeText = biomeName.getString();
+							if (biomeText.contains(".")) biomeText = biomeText.substring(biomeText.lastIndexOf(".")+1);
+							if (biomeText.contains("_")) {
+								biomeText = biomeText.replace("_", " ").trim();
+								boolean capitalize = true;
+								for (int i=0; i<biomeText.length()-1; i++) {
+									if (capitalize) {
+										biomeText = biomeText.substring(0, i) + biomeText.substring(i, i+1).toUpperCase() + biomeText.substring(i+1);
+										capitalize = false;
+									}
+									if (biomeText.substring(i, i+1).equals(" ")) capitalize = true;
+								}
+							}
 							matrixStack.push();
 							matrixStack.scale(1,1,1);
-							mc.textRenderer.drawWithShadow(matrixStack, biomeName, 3, 3, 0xFFFFFFFF);
+							mc.textRenderer.drawWithShadow(matrixStack, biomeText, 3, 3, 0xFFFFFFFF);
 							matrixStack.pop();
 						}
 					}
